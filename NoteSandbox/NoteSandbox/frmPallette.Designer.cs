714ed2a6150685c36d89bc0ad3f7918304b46dde
Christopher Graham <ushi84@gmail.com>
2017-03-05 11:13:24 -0600

    Adding files via upload
    Just putting up some code i want to have available later.

---
+++ NoteSandbox/NoteSandbox/frmPallette.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NoteSandbox
+{
+    partial class frmPallette
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Location = new System.Drawing.Point(5, 511);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClose.Location = new System.Drawing.Point(452, 511);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSave.Location = new System.Drawing.Point(86, 511);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmPallette
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(529, 538);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.MinimumSize = new System.Drawing.Size(537, 565);
+            this.Name = "frmPallette";
+            this.Text = "Notes";
+            this.Load += new System.EventHandler(this.frmPallette_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmPallette_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmPallette_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmPallette_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.frmPallette_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ NoteSandbox/NoteSandbox/frmPallette.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NoteSandbox
+{
+    public partial class frmPallette : Form
+    {
+       
+        // Thanks Joe Hick for the basic form drawing setup
+        
+        bool paint = false;
+        Pen p = new Pen(Color.Black, 2);
+        Pen p2 = new Pen(Color.Transparent, 1);
+        List<List<Point>> lstDrawings = new List<List<Point>>();
+        List<Point> lstPoints;
+        List<List<Point>> lstListOfPoints = new List<List<Point>>();
+        int count = 0;
+        public frmPallette()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            lstListOfPoints.Clear();
+          this.Invalidate();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Image img = new Bitmap(this.Width, this.Height);
+            Graphics g = Graphics.FromImage(img);
+            g.FillRectangle(Brushes.White, new Rectangle(0, 0, this.Width, this.Height));
+
+            Draw(g);
+
+            try
+            {
+                img.Save(String.Format(System.Configuration.ConfigurationManager.AppSettings["SavePath"] + "Notes_{0}_{1}.png", DateTime.Now.ToString("yyyy_MM_dd"),count));
+                MessageBox.Show("Saved!");
+                count++;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void frmPallette_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmPallette_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Draw(g);
+        }
+
+
+        private void frmPallette_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            lstPoints = new List<Point>();
+            lstListOfPoints.Add(lstPoints);
+        }
+
+        private void frmPallette_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+        }
+
+        private void frmPallette_MouseMove(object sender, MouseEventArgs e)
+        {
+            //bool continuous = false;
+            if (paint)
+            {
+               
+                Point p = new Point(e.X, e.Y);
+                lstPoints.Add(p);
+                this.Invalidate();
+            }
+            else
+            {
+                //lstPoints.Add(Point.Empty);
+            }
+        }
+        private void Draw(Graphics g)
+        {
+            
+            foreach (var lst in lstListOfPoints)
+            {
+                Point prevPt = Point.Empty;
+                foreach (Point pt in lst)
+                {
+                    if (prevPt != Point.Empty)
+                    {
+                        g.DrawLine(p, prevPt.X, prevPt.Y, pt.X, pt.Y);
+
+                    }
+                    else
+                    {
+                        g.DrawLine(p2, prevPt.X, prevPt.Y, pt.X, pt.Y);
+                    }
+                    prevPt = pt;
+                }
+                
+            }
+        }
+    }
+}
